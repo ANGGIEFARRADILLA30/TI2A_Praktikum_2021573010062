195b4c6a1ee7cf85faf4783a448bc91b44320887
ANGGIEFARRADILLA30 <anggiefarradilla@gmail.com>
2022-09-27 15:16:06 +0700

    PRAKTIKUM2

---
+++ praktikum2/if2.php
@@ -0,0 +1,11 @@
+<html>
+    <body>
+        <?php
+            $d=date("D"); if ($d=="saat")
+            echo "Selamat berakhir pekan!";
+            elseif ($d=="Fri")
+            echo "Selamat Menunaikan Sholat jum'at bagi yang muslim "; else
+            echo "Selemat belajar";
+        ?>
+    </body>
+</html>
---
+++ praktikum2/while.php
@@ -0,0 +1,15 @@
+<?php
+    echo"<H3>cara 1</H3>";
+    $i =1;
+    while ($i<=10)
+    {
+        print $i++;
+    }
+    echo"<BR>";
+    echo"<H3>cara 2</HR>";
+    $i=1;
+    while($i <=10):
+        print $i;
+        $i++;
+    endwhile;
+    ?>
