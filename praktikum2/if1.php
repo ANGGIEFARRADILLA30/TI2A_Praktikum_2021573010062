7c028878f5dc3a97b3c6079d646c6aa43121355b
ANGGIEFARRADILLA30 <anggiefarradilla@gmail.com>
2022-10-04 23:25:10 +0700

    praktikum3

--- praktikum2/if1.php
+++ praktikum2/if1.php
@@ -1,15 +1,22 @@
-<html>
-    <body>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
         <?php
-        date_default_timezone_set('asia/jakarta');
-        $d=date("D");
-        $date=date("d-m-Y H:i:s");
-        if ($d== "mon") {
-            $d="senin";
-            echo "selamat belajar <br>";
+        date_default_timezone_set('Asia/Jakarta');
+        $d = date ("D");
+        $date = date("d-m-Y H:i:s");
+        if ($d == "Mon") {
+            $d = "Senin";
+            echo "Selamat belajar <br>";
         } else
-            echo "ini hari $d <br>";
-        echo $d ." ". $date;
+            echo "Ini hari $d <br>";
+            echo $d." ". $date;
         ?>
     </body>
-</html>
+</html>    
