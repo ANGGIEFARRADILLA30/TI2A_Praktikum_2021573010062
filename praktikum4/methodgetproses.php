c28a2b56895db7ff0312102203131d701309dd18
ANGGIEFARRADILLA30 <anggiefarradilla@gmail.com>
2022-10-06 08:50:30 +0700

    praktukum3

---
+++ praktikum4/methodgetproses.php
@@ -0,0 +1,9 @@
+<html>
+     <head> 
+        <title> Method GET proses </itle> 
+    </head> 
+    <body> 
+        Nama yang anda inputkan adalah: <?echo
+         $_GET["nama"]?> 
+    </body> 
+</html>
