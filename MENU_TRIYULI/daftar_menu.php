4d5391960319e5281b009d255f7bdf4c82a17acf
ANGGIEFARRADILLA30 <100070428+ANGGIEFARRADILLA30@users.noreply.github.com>
2022-12-11 01:18:49 +0700

    Add files via upload

---
+++ MENU_TRIYULI/daftar_menu.php
@@ -0,0 +1,237 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="index.css">
+    <link rel="stylesheet" type="text/css" href="fontawesome/css/all.min.css">
+
+    <title>TRIYULIBAKSO</title>
+  </head>
+  <body>
+  
+      <div class="jumbotron jumbotron-fluid text-center">
+        <div class="container">
+          <h1 class="display-50"><span class="font-weight-bold">TRIYULIBAKSO</span></h1>
+          <hr>
+          <p class="lead font-weight-bold">Silahkan Pesan Menu Sesuai Keinginan Anda <br> 
+          Enjoy Your Meal</p>
+        </div>
+      </div>
+ 
+
+  <!-- Navbar -->
+      <nav class="navbar navbar-expand-lg  bg-warning">
+        <div class="container">
+        <a class="navbar-brand text-white" href="index.php"><strong>TRI</strong>YULIBAKSO</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link mr-4" href="index.php">HOME</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link mr-4" href="daftar_menu.php">DAFTAR MENU</a>
+            </li>
+          </ul>
+        </div>
+       </div> 
+      </nav>
+  <!-- Akhir Navbar -->
+
+  <!-- Menu -->
+      <div class="container">
+        <a href="#" class="btn btn-warning mt-3">PILIHAN MENU</a>
+        <div class="row mt-3">
+          <div class="col-md-3">
+            <div class="card border-warning">
+              <img src="images/baksourat.JPG" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title font-weight-bold">Bakso Urat</h5>
+               <label class="card-text harga">Rp. 18.000</label><br>
+                <a href="#" class="btn btn-primary btn-sm">MIE</a>
+                <a href="#" class="btn btn-danger btn-sm">NO MIE</a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-3">
+            <div class="card border-warning">
+              <img src="images/kepiting2.JPG" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title font-weight-bold">Bakso Kepiting</h5>
+               <label class="card-text">Rp. 70.000</label><br>
+               <a href="#" class="btn btn-primary btn-sm">MIE</a>
+                <a href="#" class="btn btn-danger btn-sm">NO MIE</a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-3">
+            <div class="card border-warning">
+              <img src="images/baksomercon.JPG" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title font-weight-bold">Bakso Mercon</h5>
+                <label class="card-text">Rp. 25.000</label><br>
+                <a href="#" class="btn btn-primary btn-sm">MIE</a>
+                <a href="#" class="btn btn-danger btn-sm">NO MIE</a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-3">
+            <div class="card border-warning">
+              <img src="images/selimutkabut.JPG" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title font-weight-bold">Selimut Kabut</h5>
+                <label class="card-text">Rp. 25.000</label><br>
+                <a href="#" class="btn btn-primary btn-sm">MIE</a>
+                <a href="#" class="btn btn-danger btn-sm">NO MIE</a>
+              </div>
+            </div>
+          </div>
+         </div>  
+
+         <div class="row mt-3">
+          <div class="col-md-3">
+            <div class="card border-warning">
+              <img src="images/tetelan.JPG" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title font-weight-bold">Bakso Tetelan</h5>
+                <label class="card-text">Rp. 20.000</label><br>
+                <a href="#" class="btn btn-primary btn-sm">MIE</a>
+                <a href="#" class="btn btn-danger btn-sm">NO MIE</a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-3">
+            <div class="card border-warning">
+              <img src="images/tehobeng.jpg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title font-weight-bold">Teh</h5>
+                <label class="card-text">Rp. 5.000</label><br>
+                <a href="#" class="btn btn-primary btn-sm">DINGIN</a>
+                <a href="#" class="btn btn-danger btn-sm">HANGAT</a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-3">
+            <div class="card border-warning">
+              <img src="images/juspukat.jpg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title font-weight-bold">Jus Alpukat</h5>
+                <label class="card-text">Rp. 10.000</label><br>
+                <a href="#" class="btn btn-primary btn-sm">DINGIN</a>
+                <a href="#" class="btn btn-danger btn-sm">HANGAT</a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-3">
+            <div class="card border-warning">
+              <img src="images/esjeruk.jpg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title font-weight-bold">jus Jeruk</h5>
+                <label class="card-text">Rp. 8.000</label><br>
+                <a href="#" class="btn btn-primary btn-sm">DINGIN</a>
+                <a href="#" class="btn btn-danger btn-sm">HANGAT</a>
+              </div>
+            </div>
+          </div>
+         </div>  
+
+         <div class="row mt-3 mb-5">
+          <div class="col-md-3">
+            <div class="card border-warning">
+              <img src="images/sanford.jpg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title font-weight-bold">Mineral</h5>
+                <label class="card-text">Rp. 5.000</label><br>
+                <a href="#" class="btn btn-primary btn-sm">DINGIN</a>
+                <a href="#" class="btn btn-danger btn-sm">TIDAK</a>
+              </div>
+            </div>
+          </div>  
+
+          <div class="col-md-3">
+            <div class="card border-warning ">
+              <img src="images/nasi.jpeg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title font-weight-bold">Nasi Putih</h5>
+                <label class="card-text">Rp. 3.000</label><br>
+                <a href="#" class="btn btn-primary btn-sm">DOUBLE</a>
+                <a href="#" class="btn btn-danger btn-sm">TIDAK</a>
+              </div>
+            </div>
+          </div>        
+
+          <div class="col-md-3">
+            <div class="card border-warning">
+              <img src="images/jusmangga.jpg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title font-weight-bold">Jus Mangga</h5>
+                <label class="card-text">Rp. 8.000</label><br>
+                <a href="#" class="btn btn-primary btn-sm">DINGIN</a>
+                <a href="#" class="btn btn-danger btn-sm">HANGAT</a>
+              </div>
+            </div>
+          </div>   
+
+          <div class="col-md-3">
+            <div class="card border-warning">
+              <img src="images/mieayam.jpg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title font-weight-bold">Mie Ayam</h5>
+                <label class="card-text">Rp. 17.000</label><br>
+                <a href="#" class="btn btn-primary btn-sm">MIE</a>
+                <a href="#" class="btn btn-danger btn-sm">NO MIE</a>
+              </div>
+            </div>
+          </div>   
+         </div> 
+      </div>
+  <!-- Akhir Menu -->
+
+  <!-- Awal Footer -->
+      <hr class="footer">
+      <div class="container">
+        <div class="row footer-body">
+          <div class="col-md-6">
+          <div class="copyright">
+            <strong>TRIYULIBAKSO</strong> <i class="far fa-copyright"></i>since 2006</p>
+          </div>
+          </div>
+
+          <div class="col-md-6 d-flex justify-content-end">
+          <div class="icon-contact">
+          <a href="#"><img src="images/icon/ig.png" class="mr-1" data-toggle="tooltip" title="Instagram"></a>
+          <label class="font-weight-bold">@triyulibakso</label>
+        </div>
+          </div>
+        </div>
+      </div>
+  <!-- Akhir Footer -->
+
+
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.js"></script>
+  </body>
+</html>
